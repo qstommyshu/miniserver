6655173c542fbc5a603f13da2033adefb5340648
qstommyshu <qstommyshu@gmail.com>
2023-04-27 09:39:29 -0400

    request handler reconstruction

--- src/main.rs
+++ src/main.rs
@@ -23,21 +23,32 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("{:#?}", request_line);
     // if get method and http 1.1, return the following
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("src/hello.html").unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("src/404.html").unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let filepath = format!("src/{filename}");
+    let contents = fs::read_to_string(filepath).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+    // if request_line == "GET / HTTP/1.1" {
+    //     let status_line = "HTTP/1.1 200 OK";
+    //     let contents = fs::read_to_string("src/hello.html").unwrap();
+    //     let length = contents.len();
+    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // } else {
+    //     let status_line = "HTTP/1.1 404 NOT FOUND";
+    //     let contents = fs::read_to_string("src/404.html").unwrap();
+    //     let length = contents.len();
+    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    //     stream.write_all(response.as_bytes()).unwrap();
+    // }
 }
 
 // fn handle_connection(mut stream: TcpStream) {
