c474d600e8e360e4038035b88ba8ab5ccee55883
qstommyshu <qstommyshu@gmail.com>
2023-04-27 09:56:08 -0400

    request handler reconstruction

--- src/main.rs
+++ src/main.rs
@@ -35,21 +35,21 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 
-// fn handle_connection(mut stream: TcpStream) {
-//     let buf_reader = BufReader::new(&mut stream);
-//     let http_request: Vec<_> = buf_reader
-//         .lines()
-//         .map(|result| result.unwrap())
-//         .take_while(|line| !line.is_empty())
-//         .collect();
-//     // println!("Request: {:#?}", http_request);
-//     let status_line = "HTTP/1.1 200 OK";
-//     let contents = fs::read_to_string("src/hello.html").unwrap();
-//     let length = contents.len();
+    // fn handle_connection(mut stream: TcpStream) {
+    //     let buf_reader = BufReader::new(&mut stream);
+    //     let http_request: Vec<_> = buf_reader
+    //         .lines()
+    //         .map(|result| result.unwrap())
+    //         .take_while(|line| !line.is_empty())
+    //         .collect();
+    //     // println!("Request: {:#?}", http_request);
+    //     let status_line = "HTTP/1.1 200 OK";
+    //     let contents = fs::read_to_string("src/hello.html").unwrap();
+    //     let length = contents.len();
 
-//     // \r\n means enter and then newline, this is the newline method for *nix
-//     // let response = "HTTP/1.1 200 OK\r\n\r\n";
-//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-//     // write_all takes [&u8] as params, so response needs to be sent as bytes
-//     stream.write_all(response.as_bytes()).unwrap();
-// }
+    //     // \r\n means enter and then newline, this is the newline method for *nix
+    //     // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    //     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    //     // write_all takes [&u8] as params, so response needs to be sent as bytes
+    //     stream.write_all(response.as_bytes()).unwrap();
+}
